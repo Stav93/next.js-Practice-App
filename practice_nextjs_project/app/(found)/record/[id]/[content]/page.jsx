3f3b7e3e1756e0bddc6950720945a5ea728c62bb
Stav <stavush93@gmail.com>
2023-08-14 21:44:37 +0300

    nested dynamic segment

---
+++ practice_nextjs_project/app/(found)/record/[id]/[content]/page.jsx
@@ -0,0 +1,23 @@
+import { RECORDS, CONTENT_PATHS, RECORDS_MAP } from "../data"
+
+export async function generateStaticParams() {
+  const recordSegmaenMaps = []
+
+  RECORDS.forEach((record) => {
+    CONTENT_PATHS.forEach((contentPath) => {
+      recordSegmaenMaps.push({id: record.id, content: contentPath})
+    })
+  })
+
+  return recordSegmaenMaps;
+}
+
+export default function Page({params: {id, content}}) {
+  const contentJsx = RECORDS_MAP[id][content];
+  //in-propertuity-id.gear
+  //in-propertuity-id.content
+  //poison-worms-id-id.gear
+  //poison-worms-id-id.content
+
+  return contentJsx;
+}
