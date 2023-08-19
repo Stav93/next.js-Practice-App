7b243d7eb3e2d112ce07e01972c8a39871884f5e
Stav <stavush93@gmail.com>
2023-08-19 16:59:17 +0300

    adding the PlayRow component

--- practice_nextjs_project/app/(found)/discography/page.jsx
+++ practice_nextjs_project/app/(found)/discography/page.jsx
@@ -2,11 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { RECORDS } from "../record/[id]/data";
 
+function PlayRow({title, href, iconImage}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      className="flex flex-row justify-center p-3 hover:bg-sky-100 border-t-[2px] border-slate-200"
+    >
+      <span className="mr-1">Play On</span>
+      <span className="relative w-[80px] h-[24px]">
+        <Image src={iconImage} alt={title} fill sizes="80px"/>
+      </span>
+    </a>
+  )
+}
+
 export default function Page() {
   return (
     <div>
       {RECORDS.map(
-        ({ id, title, date, blurb, albumImage, recordHalfImage }) => (
+        ({
+          id,
+          title,
+          date,
+          blurb,
+          albumImage,
+          recordHalfImage,
+          spotifyLink,
+          appleMusicLink,
+          youtubeMusicLink 
+          }) => (
           <div key={id}>
             <div className="w-[330px] lg:w-[500px] bg-white p-5 lg:p-10 text-left ">
               <Link
@@ -41,6 +66,22 @@ export default function Page() {
               {title}
               <div className="text-justify text-md lg:-text-lg">{blurb}</div>
               <div className="text-sm text-slate-600">Release date: {date}</div>
+              <br/>
+              <PlayRow
+                title="Spotify"
+                href={spotifyLink}
+                iconImage="/spotify_icon_500.png"
+              />
+              <PlayRow
+                title="Apple Music"
+                href={appleMusicLink}
+                iconImage="/apple_music_icon_500.png"
+              />
+              <PlayRow
+                title="Youtube Music"
+                href={youtubeMusicLink}
+                iconImage="/youtube_music_icon_500.png"
+              />
             </div>
             <br />
           </div>
